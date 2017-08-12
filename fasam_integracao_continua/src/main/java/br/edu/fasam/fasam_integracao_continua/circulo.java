eed9a38cb30b195fcabb4433afd5f44eadc267d3
ALUNO <ALUNO@192.168.1.178>
2017-08-12 14:58:10 -0300

    circulo e quadrado

---
+++ fasam_integracao_continua/src/main/java/br/edu/fasam/fasam_integracao_continua/circulo.java
@@ -0,0 +1,14 @@
+package br.edu.fasam.fasam_integracao_continua;
+
+public class circulo {
+	private double raio;
+	
+	public circulo(double raio) {
+		this.raio = raio;
+	} 
+	
+	public double getArea() {
+		return 2;
+	}
+
+}
---
+++ fasam_integracao_continua/src/main/java/br/edu/fasam/fasam_integracao_continua/quadrado.java
@@ -0,0 +1,13 @@
+package br.edu.fasam.fasam_integracao_continua;
+
+public class quadrado {
+	private double lado;
+	
+	public quadrado(double lado) {
+		this.lado = lado;
+	}
+	
+	public double getArea() {
+		return 2;
+	}
+}
