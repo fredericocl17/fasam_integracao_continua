a451383316133bfd2b1dbd9b2dbdad418e74031e
fredericocl17 <fredericocarvalholeite@gmail.com>
2017-08-12 13:58:24 -0300

    adicionando arquivos iniciais do projeto

---
+++ fasam_integracao_continua/src/main/java/br/edu/fasam/fasam_integracao_continua/App.java
@@ -0,0 +1,13 @@
+package br.edu.fasam.fasam_integracao_continua;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
